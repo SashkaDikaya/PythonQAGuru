56eddff42c52a66aa7b2f088e81f9adef237be9d
SashkaDikaya <dikayaav@gmail.com>
2022-06-25 21:09:05 +0300

    add test_google.py

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,6 +1,21 @@
 from selene import have
 from selene.support.shared import browser
+from selenium.webdriver.chrome import options
 
 browser.open('https://www.google.com/')
+options.add_argument('window-size=1920x1080');
 browser.element('[name="q"]').type('selene').press_enter()
 browser.element('#search').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def find_selene_positive_test():
+    browser.open('https://www.google.com/')
+    options.add_argument('window-size=1920x1080');
+    browser.element('[name="q"]').type('selene').press_enter()
+    browser.element('#search').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def find_selene_negative_test():
+    search = '94858gfjhjnbx.f,cb'
+    browser.element('[name="q"]').type(search).press_enter()
+    browser.element('#search').should(have.text('По запросу ' + search + ' ничего не найдено.'))
