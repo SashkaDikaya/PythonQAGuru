c10eaf0559655df89c144fab0fab1f9ea77d2620
SashkaDikaya <dikayaav@gmail.com>
2022-07-04 22:40:21 +0300

    some changes

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(autouse=True)
 def open_browser():
     browser.open('https://www.google.com/')
     browser.driver.set_window_size(1920, 1080)
